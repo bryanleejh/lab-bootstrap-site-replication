10c602f8124bb3db417dacb9c127edb31e40d4e5
Bryan Lee <bryan.lee.jh@gmail.com>
2017-05-18 12:42:01 +0800

    initialupdate

---
+++ functions.js
@@ -0,0 +1,57 @@
+// Question 1
+function maxOfTwoNumbers() {
+
+}
+
+// Question 2
+function maxOfThree() {
+
+}
+
+// Question 3
+function isCharacterAVowel() {
+
+}
+
+// Question 4
+function sumArray() {
+ 
+}
+
+
+// Question 4
+function multiplyArray() {
+
+}
+
+
+// Question 5
+var reverseString = function (){
+  
+};
+
+
+// Question 8
+function findLongestWord () {
+  
+}
+
+// Question 9
+function filterLongWords () {
+  
+}
+
+
+// Bonus 1
+//??????
+
+
+// Bonus 2
+function charactersOccurencesCount() {
+  
+}
+
+// Bonus 3
+var numberOfArguments = function(){
+  
+}
